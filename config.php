cea59e6ae59a38e0933943783ebacaaf5e2bafc8
vincedotnet <vincedotnet@hotmail.com>
2017-12-07 14:45:34 +0000

    1

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://rockpapa.gangstra.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://rockpapa.gangstra.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/rockpapa_master/catalog/');
+define('DIR_SYSTEM', '/var/www/rockpapa_master/system/');
+define('DIR_IMAGE', '/var/www/rockpapa_master/image/');
+define('DIR_LANGUAGE', '/var/www/rockpapa_master/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/rockpapa_master/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/rockpapa_master/system/config/');
+define('DIR_CACHE', '/var/www/rockpapa_master/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/rockpapa_master/system/storage/download/');
+define('DIR_LOGS', '/var/www/rockpapa_master/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/rockpapa_master/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/rockpapa_master/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'hudroot31');
+define('DB_DATABASE', 'rockpapa_master');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
